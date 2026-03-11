341d77071b96e2b3f65841438f8dcd08e62b4b09
Zaid Qtaish <enderzaide@gmail.com>
2026-03-11 16:51:05 +0300

    feat: created main.go and static frontend files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module logistock-app
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+	http.ListenAndServe(":8080", nil)
+	fmt.Println("Server started at http://localhost:8080")
+}
