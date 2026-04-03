e28dc4c31eb55586aa2c6e95a75d84dfb621b431
Zaid Qtaish <enderzaide@gmail.com>
2026-04-03 20:08:32 +0300

    update: update schema phone number to not nu

--- Project Documentation/schema.sql
+++ Project Documentation/schema.sql
@@ -20,7 +20,7 @@ CREATE TABLE users (
     role VARCHAR(50) NOT NULL,
     username VARCHAR(50) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(20)
+    phone_number VARCHAR(20) NOT NULL CHECK (btrim(phone_number) <> '')
 );
 
 -- Create Items table
